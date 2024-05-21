d72749e7ad79fdae6ea0923196ebd56dcbb66d2e
Narvik1221 <107913962+Narvik1221@users.noreply.github.com>
2024-05-21 08:52:45 +0600

    123

--- main.js
+++ main.js
@@ -43,3 +43,44 @@ window.addEventListener(
   true
 );
 
+
+document.getElementById('openTelegram').addEventListener('click', function() {
+  // Telegram username or bot
+  const telegramUsername = '@evrookna_germes_bot';
+
+  // Try to open in Telegram app
+  const appLink = `tg://resolve?domain=${telegramUsername}`;
+
+  // Fallback to open in browser
+  const webLink = `https://web.telegram.org/a/#7063673396`;
+
+  // Create an invisible iframe to try opening the app
+  const iframe = document.createElement('iframe');
+  iframe.style.display = 'none';
+  iframe.src = appLink;
+  document.body.appendChild(iframe);
+
+  // Set a timeout to check if the app was opened
+  setTimeout(() => {
+      // Remove the iframe after timeout
+      document.body.removeChild(iframe);
+      // Open in browser if app not opened
+      window.open(webLink, '_blank');
+  }, 2500);
+});
+
+
+document.getElementById('openWhatsApp').addEventListener('click', function() {
+  // Телефонный номер или текст для открытия в WhatsApp
+  const phoneNumber = '+79622842222';  // Замените на необходимый номер
+  const appLink = `whatsapp://send?phone=${phoneNumber}`;
+
+  // Создаем невидимый iframe для попытки открыть приложение
+  const iframe = document.createElement('iframe');
+  iframe.style.display = 'none';
+  iframe.src = appLink;
+  document.body.appendChild(iframe);
+
+  // Устанавливаем таймаут для проверки, открылось ли приложение
+
+});
